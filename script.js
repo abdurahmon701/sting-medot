d59d7e2d057aa72ef73edcbcd525ae06356d8d10
abdurahmon70101~ <abdurahmonabdumavlonov750@gmail.com>
2024-08-22 00:23:27 +0500

    sc ij r

---
+++ script.js
@@ -0,0 +1,42 @@
+// let a = 'Akromjon'
+
+// console.log(a.toLowerCase());
+
+// console.log(a.charAt(0));
+
+// console.log(a.slice(2,7));
+
+
+
+
+// let b = 'salom'
+// let c = 'qalesan'
+
+// let d = b.concat(' ',c)
+// console.log(d);
+
+
+// let e = 'hello world'
+
+
+// console.log(e.replace('world','hi'));
+
+
+// let f = 'salom'
+
+
+// alert(f.indexOf('m'));
+
+let a = prompt('Ismingizni kiriting')
+
+let b = prompt('holagan harf kiriting')
+
+if(a.charAt(0,9) == b){
+    alert('siz yozgan ismda shu harf bor')
+}else{
+    alert('siz yozgan ismda unday harf yoq')
+}
+// if (a.slice(0,9) == b ) {
+
+//     alert('siz yozgan ismda shu harf bor')
+// }
